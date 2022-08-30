a312d9dd1c9a3dce822a3360a45cb453d58ec338
ikbal <ikballukmanulhakim5@gmail.com>
2022-08-30 15:47:21 +0700

    graceull exit and redeliver unacknownlage event

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,26 @@
-import nats,{Message} from "node-nats-streaming";
-import {randomBytes} from "crypto";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-console.clear()
+console.clear();
 const stan = nats.connect("ticketing", randomBytes(8).toString("hex"), {
   url: "http://127.0.0.1:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const subscription = stan.subscribe("ticket:created", "orders-service-queue-group",options);
-  subscription.on("message", (msg:Message) => {
-    console.log("Message received ",msg.getSequence(),"=>",msg.getData());
-    msg.ack()
+
+  stan.on("close", () => {
+    console.log("NATS connection close");
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true).setDeliverAllAvailable().setDurableName("order-service");
+  const subscription = stan.subscribe("ticket:created", "orders-service-queue-group", options);
+  subscription.on("message", (msg: Message) => {
+    console.log("Message received ", msg.getSequence(), "=>", msg.getData());
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
