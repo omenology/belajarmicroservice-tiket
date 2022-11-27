48cd24ee04e010a31bfcf7b540f95c47cd0d622c
ikbal <ikballukmanulhakim5@gmail.com>
2022-11-27 15:03:46 +0700

    listener

--- order/src/index.ts
+++ order/src/index.ts
@@ -11,12 +11,14 @@ const start = async () => {
   if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defind");
   if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defind");
   if (!process.env.NATS_URL) throw new Error("MONGO_URL must be defind");
+  
   try {
     await natsClient.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     natsClient.stan.on("close", () => {
       console.log("NATS connection close");
       process.exit();
     });
+
     process.on("SIGINT", () => {
       natsClient.stan.close();
     });
